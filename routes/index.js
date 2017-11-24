c058ab2cc5aca1e9625c876be6a42553670f20be
FearDread <ghaptonstall@gmail.com>
2017-11-24 09:13:40 -0700

    --update assets with latest rubick theme

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,31 @@
+var utils, pubdir;
+
 
 exports.add = function (app) {
 
+  path = '../';
+  utils = require(path + 'src/utils');
+
 	app.use( function (req, res, next) {
-		console.log('all routes hit');
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept');
 
 		next();
 
 	});
 
 	app.get('/', function (req, res) {
+    console.log(utils);
 		console.log('render root');
 
 		res.render('landing', {});
+	});
+	
+	app.get('/portfolio', function (req, res) {
+		console.log('render portfolio page');
 
+		res.render('portfolio', {});
 	});
 
 }
