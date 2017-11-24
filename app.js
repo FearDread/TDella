d0d56a6fb6eb855525661e7a08aed2f5a7a54dec
Garrett Haptonstall <ghap@GBIRD-MOTHERSHIP.(none)>
2017-11-24 04:58:28 -0700

    --update main app.js with basic site and added module dependencies to package.json

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+var server, host, port;
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 
 
@@ -13,10 +15,24 @@ pubdir = __dirname;
 app.use(bodyParser.urlencoded({
 	extended: true
 	}
-);
+));
 app.use(bodyParser.json());
 
 app.set('views', pubdir, '/public/views');
 app.set('view engine', 'jade');
+
+app.use(less(path.join(pubdir, '/src', 'less'), {
+	dest: path.join(pubdir, '/public', 'css')
+	}
+));
+
+server = app.listen(4001, function () {
+	host = server.address().address;
+	port = server.address().port;
+
+	console.log('TDELLA RUNNING');
+	}
+);
+
 
 
