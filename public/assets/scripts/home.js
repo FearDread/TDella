69941dae4b2e3f32d517e99d114675017eaccb40
FearDread <ghaptonstall@gmail.com>
2017-12-24 02:39:25 -0700

    --finish needed tdella and home script logic and apply to home view

--- public/assets/scripts/home.js
+++ public/assets/scripts/home.js
@@ -13,25 +13,130 @@ $.GUI().create('Home', function (gui) {
     load: function () {
       gui.log('Load method called in home.js :', TDella);
 
-      TDella.Browser.init();
+      $(document).ready( function () {
+        TDella.Browser.init();
+    
+        if (TDella.Browser.browser == 'Explorer' && TDella.Browser.version <= 9) {
+          $('body').html(better_browser);   
+        }
 
-      if ( TDella.Browser.type == 'Explorer' && TDella.Browser.version <= 9 ) {
-          $('body').html( TDella.Browser.better_browser );
-      }
+        window_width = $(window).width();
+        window_height = $(window).height();
 
-      window_width = $(window).width();
-      window_height = $(window).height();
+        burger_menu = $('nav[role="navigation"]').hasClass('navbar-burger') ? true : false;
+    
+        if ( !Modernizr.touch ) {
 
-      menu = $('nav[role="navigation"]').hasClass('navbar-burger') ? true : false;
+          $('body').addClass('no-touch');
+          no_touch_screen = true;
+        }
+    
+        TDella.initAnimationsCheck();
 
-      TDella.init.animations();
+        if ( window_width < 992 || burger_menu ) {
+          TDella.initRightMenu();   
+        }
 
-      if ( window_width < 952 || menu ) {
-        TDella.init.menu();
-      }
+        if ( window_width < 992 ) {
+          $('.over-area').each(function () {
 
-      TDella.checks.responsive();
-      TDella.init.home();
+            var click = $(this).attr("onClick"); 
+
+            if ( click == '' ) {
+              src = "rubik.showModal(this)";
+              $(this).attr("onClick", src);
+            }
+          });
+        
+          TDella.checkResponsiveImage();
+        }
+    
+        setTimeout(function () {
+          $('.loading').css('opacity','0');
+
+          setTimeout(function(){
+            $('.loading').addClass('hide');
+          }, 500);
+        }, 3000);
+    
+        if ( $('#contactUsMap').length != 0 ) {
+          TDella.initGoogleMaps();   
+        }
+    
+        if ( $('.content-with-opacity').length != 0 ) {
+          TDella.opacity = 1;
+        }
+
+      });
+
+      $(window).load(function () {
+        TDella.initAnimationsCheck();
+      });  
+
+      $(window).resize(function () {
+
+        if ( $(window).width() < 992 ) {
+          TDella.initRightMenu();  
+          TDElla.checkResponsiveImage(); 
+        }
+
+        if ( $(window).width() > 992 && !burger_menu ) {
+          $('nav[role="navigation"]').removeClass('navbar-burger');
+          TDella.misc.navbar_menu_visible = 1;
+          navbar_initialized = false;
+        }
+      });
+
+      $(window).on('scroll',function () {
+        TDella.checkScrollForTransparentNavbar();    
+   
+        if ( window_width > 992 ) {
+          TDella.checkScrollForParallax();
+        }
+   
+        if ( TDella.opacity == 1 ) {
+          TDella.checkScrollForContentTransitions();
+        }
+      });
+
+      $('a[data-scroll="true"]').click(function(e){         
+        var scroll_target = $(this).data('id');
+        var scroll_trigger = $(this).data('scroll');
+    
+        if ( scroll_trigger == true && scroll_target !== undefined ) {
+          e.preventDefault();
+        
+          $('html, body').animate({
+            scrollTop: $(scroll_target).offset().top - 50
+          }, 1000);
+        }
+      });
+
+      $('.section-we-made-2 .scroller').mousemove(
+        function ( event ) {
+          if ( !Modernizr.touch ) {
+
+            if ( event.clientX < 200 ) { 
+              $(this).css("transform","translateX(0)");
+            }
+
+            if (event.clientX > 200 && event.clientX < $(window).width()-200 && event.clientX % 2 == 0 ) {
+              pixels = -event.clientX * scroll;
+              
+              $(this).css("transform","translateX(" + pixels + "px)");
+            }
+
+            if(event.clientX > $(window).width()-200) { 
+              pixels = -(2500 - $(window).width());
+              $(this).css("transform","translateX(" + pixels + "px)");
+            }
+
+            $('.projects').css('overflow','hidden');
+          }
+        }
+      );
+
+      //TDella.init.home();
     },
     unload: function () {
       gui.log('tdella unload method called');
--- public/assets/scripts/tdella.js
+++ public/assets/scripts/tdella.js
@@ -52,256 +52,63 @@ TDella = (function () {
   }
 
   return {
-    opts: {},
-    debounce: debounce,
-    init: {
-      home: function () {
-        var $box,
-            options_1,
-            options_2,
-            options_3;
+    misc:{
+      navbar_menu_visible: 0
+    },
+    initAnimationsCheck: function () {},
+    initRightMenu: function () {},
+    checkResponsiveImage: function () {
 
-        $(document).ready(function() {
-          $box = $('.box');
+      responsive_background = $('.section-header > div .responsive-background');
+        
+      if (responsive_background.length == 0) {
+           $('.section-header > div > img, .section-header video').each(function(){
 
-          options_1 = {
-            speed: 1200
-              , autoScroll: true
-              , timeout: 5000
-              , effect: 'scrollHorz3d'
-            };
-
-          options_2 = {
-            speed: 1200
-            , timeout: 1800
-            , autoScroll: true
-            , effect: 'scrollHorz3d'
-          };
-
-          options_3 = {
-            speed: 1200
-            , timeout: 6200
-            , autoScroll: true
-            , effect: 'scrollHorz3d'
-          };
-
-          $('.box-1').boxSlider(options_1);
-          $('.box-2').boxSlider(options_2);
-          $('.box-3').boxSlider(options_3);
-
-        });
-      },
-      animations: function () { 
-        $('[class*="add-animation"]').each(function(){
-           var offset_diff = 30;
-
-           if( $(this).hasClass('title') ) {
-               offset_diff = 110;
-           }
-           
-           var waypoints = $(this).waypoint(function( direction ) {
-             if ( direction == 'down' ) {
-               $(this.element).addClass('animate');    
-             } else {
-               $(this.element).removeClass('animate');
+             var $image = $(this);
+             var src = $image.attr("src");
+                
+             if ( $image.attr("responsive-src") ) {
+               src = $image.attr("responsive-src");    
              }
-           }, { offset: window_height - offset_diff });
-        });
-      },
-      menu: function () {
-         if ( !navbar_init ) {
-            $nav = $('nav[role="navigation"]');
-            $nav.addClass('navbar-burger');
-             
-            $navbar = $nav.find('.navbar-collapse').first().clone(true);
-              
-            ul_content = '';
-             
-            $navbar.children('ul').each(function(){
-                content_buff = $(this).html();
-                ul_content = ul_content + content_buff;   
-            });
-             
-            ul_content = '<ul class="nav navbar-nav">' + ul_content + '</ul>';
-            $navbar.html(ul_content);
-             
-            $('body').append($navbar);
-                            
-            background_image = $navbar.data('nav-image');
-            if(background_image != undefined){
-                $navbar.css('background',"url('" + background_image + "')")
-                       .removeAttr('data-nav-image')
-                       .css('background-size',"cover")
-                       .addClass('has-image');                
-            }
-             
-            $toggle = $('.navbar-toggle');
-             
-            $navbar.find('a').removeClass('btn btn-round btn-default');
-            $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn-success btn-danger btn-warning btn-neutral');
-            $navbar.find('button').addClass('btn-simple btn-block');
-
-            $link = $navbar.find('a');
-            
-            $link.click(function(e){
-                var scroll_target = $(this).data('id');
-                var scroll_trigger = $(this).data('scroll');
-                
-                if(scroll_trigger == true && scroll_target !== undefined){
-                    e.preventDefault();
-
-                    $('html, body').animate({
-                         scrollTop: $(scroll_target).offset().top - 50
-                    }, 1000);
-                }
-                
-             });
-
-            
-            $toggle.click(function (){    
-
-                if(rubik.misc.navbar_menu_visible == 1) {                    
-                    $('html').removeClass('nav-open'); 
-                    TDella.menu.visible = 0;
-
-                    $('#bodyClick').remove();
-                     setTimeout(function(){
-                        $toggle.removeClass('toggled');
-                     }, 550);
-                
-                } else {
-                    setTimeout(function(){
-                        $toggle.addClass('toggled');
-                    }, 580);
-                    
-                    div = '<div id="bodyClick"></div>';
-                    $(div).appendTo("body").click(function() {
-                        $('html').removeClass('nav-open');
-                        rubik.misc.navbar_menu_visible = 0;
-                        $('#bodyClick').remove();
-                         setTimeout(function(){
-                            $toggle.removeClass('toggled');
-                         }, 550);
-                    });
-                   
-                    $('html').addClass('nav-open');
-                    rubik.misc.navbar_menu_visible = 1;
-                    
-                }
-            });
-            navbar_initialized = true;
-        }
-      
-      },
-      goole: function () {}
-    },
-    showModal: function (button) {
-      var id, scrollTop, distanceTop, modal;
-      var $project, projectTop, projectLeft, projectHeight, projectWidth;
-
-      id = $(button).data('target');
-      $project = $(button).closest('.project');
-      
-      scrollTop = $(window).scrollTop();
-      distanceTop = $project.offset().top;
-
-      projectTop = distanceTop - scrollTop; 
-      projectLeft = $project.offset().left;
-      projectHeight = $project.innerHeight();
-      projectWidth = $project.innerWidth();
-
-      modal = $('#' + id);
-
-      $(modal).css({
-        'top': projectTop,
-        'left': projectLeft, 
-        'width': projectWidth,
-        'height': projectHeight,
-        'z-index': '1032'
-      }).addClass('has-background');
-      
-      // $(modal).addClass('has-background');
-      
-      setTimeout(function () {
-         $(modal).addClass('open');
-      }, 30);
-
-      setTimeout(function () {
-         $('body').addClass('noscroll');
-         $(modal).addClass('scroll');
-      }, 1000);
-  
-      $('.icon-close').click(function(){
-        $project_content = $(this).closest('.project-content');
-        $project_content.removeClass('open scroll');
-        
-        $('body').removeClass("noscroll");
-        //$('a').removeClass('no-opacity');
-
-          setTimeout(function(){
-              $project_content.removeClass('has-background');
-
-              setTimeout(function(){    
-                  $project_content.removeAttr('style');     
-              }, 450); 
-          },500);
-      });
-    },
-    checks: {
-      responsive: function () {
-        responsive_background = $('.section-header > div .responsive-background');
-        
-        if(responsive_background.length == 0){
-            $('.section-header > div > img, .section-header video').each(function(){
-                var $image = $(this);
-                var src = $image.attr("src");
-                
-                if($image.attr("responsive-src")){
-                    src = $image.attr("responsive-src");    
-                }
                 
                 div = '<div class="responsive-background" style="background-image:url(' + src + ')"/>';
                 $image.after(div);
                 $image.addClass('hidden-xs hidden-sm'); 
-            });   
+            });
         }
-      },
-      transparent: debounce ( function () {
-        if($(document).scrollTop() > 560 ) {
-              if(transparent) {
-                  transparent = false;
-                  $('nav[role="navigation"]').removeClass('navbar-transparent');
-              }
-          } else {
-              if( !transparent ) {
-                  transparent = true;
-                  $('nav[role="navigation"]').addClass('navbar-transparent');
-              }
-          }
-      }, 17),
-      parallax: debounce (function () {
-
-        $('.content-with-opacity').each(function() {
-          var $content = $(this);
-             
-          if ( isElementInViewport ( $content )) {
-            var window_top = $(window).scrollTop();
-
-            opacity = 1 - (window_top / 230);
-                  
-            if ( opacityVal < 0 ) {
-
-              opacityVal = 0;
-              return;
-
-             } else {
-
-               $content.css('opacity',opacityVal);    
-             }
-        	 }
-        });
-      }, 6, false),
-      transitions: debounce ( function () {
+    },
+    checkScrollForTransparentNavbar: debounce(function() {	
+        	if($(document).scrollTop() > 560 ) {
+                if(transparent) {
+                    transparent = false;
+                    $('nav[role="navigation"]').removeClass('navbar-transparent');
+                }
+            } else {
+                if( !transparent ) {
+                    transparent = true;
+                    $('nav[role="navigation"]').addClass('navbar-transparent');
+                }
+            }
+    }, 17),
+    
+    checkScrollForParallax: debounce(function() {	
+        	no_of_elements = 0;
+        	$('.parallax').each(function() {
+        	    var $elem = $(this);
+        	    
+        	    if(isElementInViewport($elem)){
+                  var parent_top = $elem.offset().top;          
+                  var window_bottom = $(window).scrollTop();
+                  var $image = $elem.children('img');
+                              	  
+            	  oVal = ((window_bottom - parent_top) / 3);
+                  $image.css('transform','translate3d(0px, ' + oVal + 'px, 0px)');    
+        	    }
+            });
+    		
+    }, 6),
+    
+    checkScrollForContentTransitions: debounce(function() {
          $('.content-with-opacity').each(function() {
              var $content = $(this);
              
@@ -318,55 +125,117 @@ TDella = (function () {
                       
         	    }            
          });
-      }, 6, false)
+    }, 6),
+    
+    showModal: function(button){
+        var id = $(button).data('target');
+        var $project = $(button).closest('.project');
+        
+        var scrollTop = $(window).scrollTop();
+        var distanceTop = $project.offset().top;
+
+        var projectTop = distanceTop - scrollTop; 
+        var projectLeft = $project.offset().left;
+        var projectHeight = $project.innerHeight();
+        var projectWidth = $project.innerWidth();
+
+        modal = $('#' + id);
+
+        $(modal).css({
+         'top'  :    projectTop,
+         'left' :    projectLeft, 
+         'width' :   projectWidth,
+         'height' :  projectHeight,
+         'z-index'  : '1032'
+        });
+        
+        $(modal).addClass('has-background');
+        
+        setTimeout(function(){
+           $(modal).addClass('open');
+        },30);
+
+        setTimeout(function(){
+           $('body').addClass('noscroll');
+           $(modal).addClass('scroll');
+        },1000);
+    
+        $('.icon-close').click(function(){
+          $project_content = $(this).closest('.project-content');
+          $project_content.removeClass('open scroll');
+          
+          $('body').removeClass("noscroll");
+          //$('a').removeClass('no-opacity');
+            setTimeout(function(){
+                $project_content.removeClass('has-background');
+                setTimeout(function(){    
+                    $project_content.removeAttr('style');     
+                }, 450); 
+            },500);
+        });
+    },
+    
+    initGoogleMaps: function(){
+        var myLatlng = new google.maps.LatLng(44.433530, 26.093928);
+        
+        var mapOptions = {
+          zoom: 16,
+          center: myLatlng,
+          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+          disableDefaultUI: true,
+          styles: [{"featureType":"administrative","elementType":"labels","stylers":[{"visibility":"on"},{"gamma":"1.82"}]},{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"gamma":"1.96"},{"lightness":"-9"}]},{"featureType":"administrative","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"on"},{"lightness":"25"},{"gamma":"1.00"},{"saturation":"-100"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#ffaa00"},{"saturation":"-43"},{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"},{"hue":"#ffaa00"},{"saturation":"-70"}]},{"featureType":"road.highway.controlled_access","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"on"},{"saturation":"-100"},{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"saturation":"-100"},{"lightness":"40"},{"visibility":"off"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"gamma":"0.80"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"off"}]}]
+        }
+        var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
+        
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            title:"Hello World!"
+        });
+        
+        // To add the marker to the map, call setMap();
+        marker.setMap(map);
     },
     Browser: {
       init: function () {
-        console.log('Browser object :: ', this);
-        this.browser = this.search(this.data) || "Other";
-        this.version = this.getVersion(navigator.userAgent) || this.getVersion(navigator.appVersion) || "Unknown";
+        this.browser = this.searchString(this.dataBrowser) || "Other";
+        this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "Unknown";
       },
-      search: function (data) {
-        var string, i;
+      searchString: function (data) {
 
-        for ( i = 0; i < data.length; i++ ) {
+        for (var i = 0; i < data.length; i++) {
 
-            string = data[i].string;
-            this.query = data[i].subString;
+          var dataString = data[i].string;
+          this.versionSearchString = data[i].subString;
 
-            if ( string.indexOf( data[i].subString ) !== -1 ) {
-                return data[i].identity;
-            }
+          if (dataString.indexOf(data[i].subString) !== -1) {
+            return data[i].identity;
+          }
         }
       },
-      getVersion: function (string) {
-        var index, rver;
+      searchVersion: function (dataString) {
+        var index = dataString.indexOf(this.versionSearchString);
 
-        index = string.indexOf(this.versionString);
-        if (index === -1) {
-            return;
-        }
+        if (index === -1) return;
 
-        rv = string.indexOf("rv:");
+        var rv = dataString.indexOf("rv:");
 
-        if (this.versionString === "Trident" && rv !== -1) {
-
-            return parseFloat(string.substring(rv + 3));
-
+        if (this.versionSearchString === "Trident" && rv !== -1) {
+            return parseFloat(dataString.substring(rv + 3));
         } else {
-
-            return parseFloat(string.substring(index + this.versionString.length + 1));
-
+            return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
         }
       },
-      data: [
+      dataBrowser: [
         {string: navigator.userAgent, subString: "Chrome", identity: "Chrome"},
         {string: navigator.userAgent, subString: "MSIE", identity: "Explorer"},
         {string: navigator.userAgent, subString: "Trident", identity: "Explorer"},
         {string: navigator.userAgent, subString: "Firefox", identity: "Firefox"},
         {string: navigator.userAgent, subString: "Safari", identity: "Safari"},
         {string: navigator.userAgent, subString: "Opera", identity: "Opera"}
-      ]
+      ],
+      better_browser: '<div class="container"><div class="better-browser row"><div class="col-md-2"></div><div class="col-md-8"><h3>We are sorry but it looks like your Browser doesn\'t support our website Features. In order to get the full experience please download a new version of your favourite browser.</h3></div><div class="col-md-2"></div><br><div class="col-md-4"><a href="https://www.mozilla.org/ro/firefox/new/" class="btn btn-warning">Mozilla</a><br></div><div class="col-md-4"><a href="https://www.google.com/chrome/browser/desktop/index.html" class="btn ">Chrome</a><br></div><div class="col-md-4"><a href="http://windows.microsoft.com/en-us/internet-explorer/ie-11-worldwide-languages" class="btn">Internet Explorer</a><br></div><br><br><h4>Thank you!</h4></div></div>'
     }
+
   }
+
 })();
